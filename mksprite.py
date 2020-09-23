b1a0b531f4b75560398d9ff5f0fe9bfc73ab20b1
Petr Viktorin <encukou@gmail.com>
2020-09-23 10:48:54 +0200

    Add die images

--- mksprite.py
+++ mksprite.py
@@ -2,8 +2,10 @@ from PIL import Image
 
 base = Image.open('pics.png')
 
+UNIT = 64
+
 def mksprite(x, y, w, h, name):
-    base.crop((64*x, 64*y, 64*(x+w), 64*(y+h))).save(f'mufl/images/{name}.png')
+    base.crop((UNIT*x, UNIT*y, UNIT*(x+w), UNIT*(y+h))).save(f'mufl/images/{name}.png')
 
 mksprite(0, 0, 2, 1, 'fish')
 mksprite(2, 0, 0.5, 1, 'hook')
@@ -25,3 +27,24 @@ mksprite(2, 1, 0.5, 0.5, 'magic')
 mksprite(2.5, 1, 0.5, 0.5, 'cube')
 
 mksprite(15, 1, 1, 1, 'logo')
+
+mksprite(0, 2, 1, 1, 'die_front')
+mksprite(1, 2, 1, 1, 'die_side')
+mksprite(2, 2, 1, 1, 'die_back')
+mksprite(3, 2, 1, 1, 'die_top')
+mksprite(4, 2, 1, 1, 'die_bottom')
+
+
+def mkdie():
+    margin = 16
+    side = UNIT - margin * 2
+    result = Image.new('RGBA', (side * 5, side))
+    for i in range(5):
+        c = base.crop((
+            i * UNIT + margin, 2 * UNIT + margin,
+            (i+1) * UNIT - margin, 3 * UNIT - margin,
+        ))
+        result.paste(c, (i * side, 0))
+    result.save('mufl/images/die.png')
+
+mkdie()
