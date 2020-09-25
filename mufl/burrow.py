28c9b64b99cef2e2dd33845472f5734b4b1b69bf
Petr Viktorin <encukou@gmail.com>
2020-09-25 17:36:54 +0200

    Redo card selecting to add undo

--- mufl/burrow.py
+++ mufl/burrow.py
@@ -1,30 +1,29 @@
 from random import shuffle
 from math import tau
+from enum import Enum
+from dataclasses import dataclass
 
 from wasabi2d import clock, animate, keys
 
 from .common import add_key_icon, change_sprite_image, KEY_NUMBERS, add_space_instruction
 
-CARD_COLOR = 0, 0, 0.02
-FACE_COLOR = {
-    'card_foot': (0.73, 0.0, 1.0),
-    'card_left': (0.0, 1.0, 0.85),
-    'card_right': (0.0, 0.8, 1.0),
-}
 
-ANGLES = {
-    (1, 0): 0,
-    (0, 1): tau/4,
-    (-1, 0): tau/2,
-    (0, -1): tau*3/4,
-}
+@dataclass
+class Card:
+    value: str
+    face_up: bool = False
+    current_animation: object = None
+    anim_sprite: object = None
+    origin_deck: int = 0
+    selected_index: int = 0
+    sel_sprite: object = None
+
 
 def deck_pos(i):
     return 280 + 80*i, 128-24
 
-
-def tile_pos(x, y):
-    return 304 + x * 62, 315 + y * 62
+def selected_pos(i):
+    return 32 + 20 + 24 * ((i%32)-1), 160 + 38 * (i//32)
 
 
 class Burrowing:
@@ -33,200 +32,179 @@ class Burrowing:
 
         self.bg_layer = game.scene.layers[0]
         self.deck_layer = self.tile_layer = game.scene.layers[1]
-        self.card_layer1 = self.worm_layer = game.scene.layers[2]
-        self.card_layer2 = self.hypno_layer = self.key_layer1 = game.scene.layers[3]
-        self.card_layer3 = self.key_layer2 = game.scene.layers[4]
+        self.turncard_layer = self.worm_layer = game.scene.layers[2]
+        self.selcard_layer = self.hypno_layer = self.key_layer1 = game.scene.layers[3]
+        self.key_layer2 = game.scene.layers[4]
 
-        self.deck_sprites = []
-
-        for i in range(4):
-            paper = self.deck_layer.add_sprite('card', color=CARD_COLOR, pos=deck_pos(i))
-            ink = self.card_layer1.add_sprite('card_back', pos=deck_pos(i))
-            k = add_key_icon(self.key_layer1, self.key_layer2, 280 + 80*i + 4, 24, str(i+1))
-            self.deck_sprites.append((paper, ink, *k))
-
-        self.space_sprites = add_space_instruction(self.deck_layer, 'Go')
-
-        self.selecting = False
-        self.selected = []
-        self.turnover_cancels = {}
-
-        self.tile_sprites = {}
-        for x in range(4):
-            for y in range(5):
-                self.tile_sprites[x, y] = self.tile_layer.add_sprite('block_10000', pos=tile_pos(x, y))
-
-        cards = ['card_foot'] * 22 + ['card_left'] * 20 + ['card_right'] * 20
+        cards = [Card(c) for c in ['card_foot'] * 22 + ['card_left'] * 20 + ['card_right'] * 20]
         shuffle(cards)
         q = len(cards) // 4
         self.decks = cards[0:q], cards[q:2*q], cards[2*q:3*q], cards[3*q:4*q]
-        self.decks[0].append('card_foot')
-        self.decks[1].append('card_left')
-        self.decks[2].append('card_right')
-        self.decks[3].append('card_foot')
+        self.decks[0].append(Card('card_foot'))
+        self.decks[1].append(Card('card_left'))
+        self.decks[2].append(Card('card_right'))
+        self.decks[3].append(Card('card_foot'))
 
-        self.arrow_sprite = self.hypno_layer.add_sprite('arrow_guide', color=(1, 1, 1, 0), pos=tile_pos(0, -1), angle=tau/4)
+        self.selected = []
 
-        async def init_anim():
-            await clock.coro.sleep(0.5)
-            for i in range(len(self.decks)):
-                self.turn_over(i, i/10)
-            await clock.coro.sleep(0.5)
-            await self.add_card(0)
+        self.deck_sprites = []
+        self.deck_keylabels = []
+        for i, deck in enumerate(self.decks):
+            s = self.deck_layer.add_sprite('card_back', pos=deck_pos(i))
+            k = add_key_icon(self.key_layer1, self.key_layer2, 280 + 80*i + 4, 24, str(i+1))
+            self.deck_sprites.append(s)
+            self.deck_keylabels.append(k)
+
+        self.selecting = False
+        async def start_anim():
+            for i, deck in enumerate(self.decks):
+                self.turn_deck(i)
+                await clock.coro.sleep(1/3)
+            self.update_deck_sprites()
+            self.add_card(0)
             self.selecting = True
-            clock.coro.run(self.anim_arrow())
 
-        self.paper_sprites = {}
-        self.ink_sprites = {}
+        clock.coro.run(start_anim())
 
-        clock.coro.run(init_anim())
+    def update_deck_sprites(self):
+        for i, deck in enumerate(self.decks):
+            for s in self.deck_keylabels[i]:
+                if deck:
+                    s.color = (*s.color[:3], 1)
+                else:
+                    s.color = (*s.color[:3], 0)
+            for card in reversed(deck):
+                if card.current_animation:
+                    continue
+                if card.face_up:
+                    change_sprite_image(self.deck_sprites[i], card.value)
+                    self.deck_sprites[i].color = 1, 1, 1, 1
+                else:
+                    change_sprite_image(self.deck_sprites[i], 'card_back')
+                    self.deck_sprites[i].color = 1, 1, 1, 1
+                break
+            else:
+                self.deck_sprites[i].color = 1, 1, 1, 0
 
-        pg = self.hypno_layer.add_particle_group(
-            'hypno', grow=0.9, max_age=1, spin_drag=1.1,
-        )
-        pg.add_color_stop(0, (1, 1, 1, 0))
-        pg.add_color_stop(.4, (1, 1, 1, 1))
-        pg.add_color_stop(.6, (1, 1, 1, .8))
-        pg.add_color_stop(1, (.8, .8, 1, 0))
-        self.hypno_emitters = []
-        for x in range(4):
-            for y in range(5):
-                em = pg.add_emitter(
-                    rate=1, pos=tile_pos(x, y), pos_spread=(16, 16),
-                    vel_spread=(8, 8), size=16, spin_spread=tau,
-                )
-                self.hypno_emitters.append(em)
+    def set_animation(self, card, animation):
+        if card.current_animation:
+            card.current_animation.cancel()
+        if card.anim_sprite:
+            card.anim_sprite.delete()
+            card.anim_sprite = None
+        async def wrapper():
+            try:
+                await animation
+            finally:
+                card.current_animation = None
+                if card.anim_sprite:
+                    card.anim_sprite.delete()
+                    card.anim_sprite = None
+                self.update_deck_sprites()
+        card.current_animation = clock.coro.run(wrapper())
 
-        x, y = tile_pos(0, -1)
-        self.worm_sprites = [
-            self.worm_layer.add_sprite('worm_segment', pos=(x-i*16*0.95**i, y+32*(1-0.9**i)), scale=0.9**i)
-            for i in reversed(range(10))
-        ]
-
-
-    async def anim_arrow(self):
-        while self.selecting:
-            await animate(self.arrow_sprite, color=(1, 1, 1, 0.6), duration=0.2)
-            await clock.coro.sleep(0.5)
-            await animate(self.arrow_sprite, color=(1, 1, 1, 0), duration=0.2)
-            await clock.coro.sleep(0.2)
-
-
-    def turn_over(self, i, delay=0):
+    def turn_deck(self, i):
+        """Make sure top of deck is turned face up"""
         try:
             card = self.decks[i][-1]
         except IndexError:
-            for s in self.deck_sprites[i]:
-                s.color = 0, 0, 0, 0
             return
-        x, y = pos = deck_pos(i)
-        paper = self.card_layer2.add_sprite('card', color=(0, 0, 0, 0), pos=pos)
-        ink = self.card_layer3.add_sprite('card_back', color=(0, 0, 0, 0), pos=pos)
-        self.paper_sprites[i] = paper
-        self.ink_sprites[i] = ink
+        if not card.face_up:
+            card.face_up = True
+            self.set_animation(card, self.do_flip_animation(card, i, True))
+        self.update_deck_sprites()
 
-        def maybe_hide_deck():
-            if len(self.decks[i]) <= 1:
-                for s in self.deck_sprites[i][:2]:
-                    s.color = 0, 0, 0, 0
-            if len(self.decks[i]) < 1:
-                for s in self.deck_sprites[i]:
-                    s.color = 0, 0, 0, 0
+    async def do_flip_animation(self, card, deck, to_face_up):
+        if to_face_up:
+            start_face = 'card_back'
+            end_face = card.value
+        else:
+            start_face = card.value
+            end_face = 'card_back'
+        x, y = pos = deck_pos(deck)
+        card.anim_sprite = s = self.turncard_layer.add_sprite(
+            start_face,
+            pos=pos,
+        )
+        duration = 0.5
+        async for t in clock.coro.frames(seconds=duration):
+            t /= duration
+            s.scale_x = abs(1 - 2*t)
+            s.y = y + 32 * t * (t-1)
+            if t > 0.5:
+                change_sprite_image(s, end_face)
 
-        cancelled = False
-        def cancel():
-            nonlocal cancelled
-            cancelled = True
-            paper.scale_x = ink.scale_x = 1
-            paper.y = ink.y = y
-            paper.color = CARD_COLOR
-            ink.color = (*FACE_COLOR[card], 1)
-            change_sprite_image(ink, card)
-            maybe_hide_deck()
-        async def _turn_over():
-            try:
-                if delay:
-                    await clock.coro.sleep(delay)
-                paper.color = CARD_COLOR
-                ink.color = (1, 1, 1, 1)
-                duration = 0.5
-                maybe_hide_deck()
-                async for t in clock.coro.frames(seconds=duration):
-                    if cancelled:
-                        break
-                    t /= duration
-                    paper.scale_x = ink.scale_x = abs(1 - 2*t)
-                    paper.y = ink.y = y + 32 * t * (t-1)
-                    if t > 0.5:
-                        change_sprite_image(ink, card)
-                        ink.color = FACE_COLOR[card]
-            finally:
-                cancel()
-        clock.coro.run(_turn_over())
-        self.turnover_cancels[i] = cancel
-
-    async def add_card(self, i):
+    def add_card(self, i):
         try:
             card = self.decks[i].pop()
         except IndexError:
             return
+        card.face_up = True
+        clock.schedule(lambda: self.turn_deck(i), 0.2, strong=True)
+        card.origin_deck = i
+        card.selected_index = len(self.selected)
         self.selected.append(card)
-        pos = deck_pos(i)
-        if not (paper := self.paper_sprites.pop(i, None)):
-            paper = self.card_layer2.add_sprite('card', color=CARD_COLOR, pos=pos)
-        if not (ink := self.ink_sprites.pop(i, None)):
-            print('Missing ink!')
-            ink = self.card_layer3.add_sprite(card, color=FACE_COLOR[card], pos=pos)
-        self.turn_over(i, delay=0.5)
+        self.set_animation(card, self.do_put_animation(card))
+        card.sel_sprite = self.selcard_layer.add_sprite(
+            card.value,
+            pos=selected_pos(card.selected_index),
+            color=(1, 1, 1, 0),
+            scale=0.3,
+        )
+        self.update_deck_sprites()
 
-        self.deck_sprites.append((paper, ink))
-
-        pos = self.get_selected_pos(len(self.selected)-1)
-        for s in paper, ink:
-            an = animate(s, pos=pos, scale=0.4, duration=0.5, tween='accel_decel')
-        await clock.coro.sleep(0.4)
-        for spr, sc in (paper, 0.38), (ink, 0.4):
-            an = animate(spr, pos=pos, scale=sc, duration=0.1, tween='accel_decel')
-
-        x, y, d = self.get_end_pos()
-        self.arrow_sprite.pos = tile_pos(x, y)
+    async def do_put_animation(self, card):
+        card.anim_sprite = s = self.turncard_layer.add_sprite(
+            card.value,
+            pos=deck_pos(card.origin_deck),
+        )
+        pos = selected_pos(card.selected_index)
+        anim = animate(card.anim_sprite, pos=pos, scale=0.3, duration=1, tween='accel_decel')
         try:
-            self.arrow_sprite.angle = ANGLES[d]
-            change_sprite_image(self.arrow_sprite, 'arrow_guide')
-        except KeyError:
-            change_sprite_image(self.arrow_sprite, 'nada_guide')
-            ink.color = (1, 1, 1, 0.5)
+            await anim
+        finally:
+            anim.stop(complete=True)
+            if card.sel_sprite:
+                card.sel_sprite.color = 1, 1, 1, 1
 
-    def get_selected_pos(self, i):
-        return 32 + 20 + 24 * ((i%32)-1), 160 + 38 * (i//32)
+    async def do_undo_animation(self, card):
+        card.anim_sprite = s = self.turncard_layer.add_sprite(
+            card.value,
+            pos=selected_pos(card.selected_index),
+            scale = 0.3,
+        )
+        pos = deck_pos(card.origin_deck)
+        anim = animate(card.anim_sprite, pos=pos, scale=1, duration=0.2, tween='linear')
+        try:
+            await anim
+        finally:
+            anim.stop(complete=True)
+            if card.sel_sprite:
+                card.sel_sprite.color = 1, 1, 1, 1
+
+    def undo(self):
+        try:
+            card = self.selected.pop()
+        except IndexError:
+            return
+        self.decks[card.origin_deck].append(card)
+        if card.sel_sprite:
+            card.sel_sprite.delete()
+            card.sel_sprite = None
+        self.set_animation(card, self.do_undo_animation(card))
+        self.update_deck_sprites()
 
     def on_key_down(self, key):
         if self.selecting:
             if key == keys.ESCAPE:
+                if len(self.selected) > 1:
+                    self.undo()
+                    return
                 self.game.abort_activity()
             num = KEY_NUMBERS.get(key)
             if num and 1 <= num <= len(self.decks):
                 i = num - 1
-                if cancel := self.turnover_cancels.pop(i, None):
-                    cancel()
-                clock.coro.run(self.add_card(i))
+                self.add_card(i)
             if key == keys.SPACE:
                 self.selecting = False
 
-    def get_end_pos(self):
-        pos = [0, -1]
-        d = (0, 1)
-        for card in self.selected:
-            if card == 'card_foot':
-                pos[0] += d[0]
-                pos[1] += d[1]
-            elif card == 'card_left':
-                dx, dy = d
-                d = dy, -dx
-            elif card == 'card_right':
-                dx, dy = d
-                d = -dy, dx
-            if not ((0 <= pos[0] < 4) and (0 <= pos[1] < 5)):
-                d = None
-                break
-        return (*pos, d)
